6f6dffb00fdd6260bd26507891322dda6bb87c08
Sliim <sliim@mailoo.org>
2014-07-14 05:22:16 +0200

    Bump version 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -19,7 +19,7 @@ maintainer_email 'sliim@mailoo.org'
 license          'Apache 2.0'
 description      'Installs/Configures beefproject'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'apt'
 depends 'rbenv'
