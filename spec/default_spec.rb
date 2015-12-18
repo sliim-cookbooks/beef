b16104d772d655961defe9156d804c8947e8e9e8
Sliim <sliim@mailoo.org>
2015-12-18 19:51:15 +0100

    useless code

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
 
 require 'chefspec'
-require 'chefspec/berkshelf'
-
-ChefSpec::Coverage.start! { add_filter 'beef' }
-
-require 'chef/application'
 
 describe 'beef::default' do
   context 'with custom user' do
